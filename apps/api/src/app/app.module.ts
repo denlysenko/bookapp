c69042bb6e7f564929c6a29b2fafd8264675e2da
denlysenko <den.lysenko.mail@gmail.com>
2019-07-14 18:07:41 +0300

    add imports to app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-
+import { AuthModule } from '@bookapp/api/auth';
+import { ConfigModule } from '@bookapp/api/config';
+import { DatabaseModule } from '@bookapp/api/database';
 import { GraphqlModule } from '@bookapp/api/graphql';
 
+import { Module } from '@nestjs/common';
+
 @Module({
-  imports: [GraphqlModule]
+  imports: [ConfigModule, DatabaseModule, GraphqlModule, AuthModule]
 })
 export class AppModule {}
