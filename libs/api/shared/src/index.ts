dd1777e6cebd8d1bdb9d1179e82b3a712ead255b
denlysenko <den.lysenko.mail@gmail.com>
2019-07-14 18:28:36 +0300

    add roles decorator, auth, roles guards

--- libs/api/shared/src/index.ts
+++ libs/api/shared/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/models/api-query';
+export * from './lib/decorators/roles';
+export * from './lib/guards/auth';
+export * from './lib/guards/roles';
---
+++ libs/api/shared/src/lib/guards/roles.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+
+    const ctx = GqlExecutionContext.create(context);
+    const request = ctx.getContext().req;
+    const user = request.user;
+    const hasRole = () => user.roles.some(role => roles.includes(role));
+    return user && user.roles && hasRole();
+  }
+}
