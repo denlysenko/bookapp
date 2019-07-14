c38a9497bbb5afbc57208ab9fb71dfa4d2e63de5
denlysenko <den.lysenko.mail@gmail.com>
2019-07-14 18:09:19 +0300

    remove dotenv config from main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,4 @@
-// tslint:disable-next-line: no-var-requires
-require('dotenv').config();
-
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
