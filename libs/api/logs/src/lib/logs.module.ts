7a389c5ceaabb0b9a9008284d657f4c265055992
denlysenko <den.lysenko.mail@gmail.com>
2019-08-04 11:02:33 +0300

    use constants as model names

--- libs/api/logs/src/lib/logs.module.ts
+++ libs/api/logs/src/lib/logs.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { LOG_MODEL_NAME } from './constants';
 import { LogsResolver } from './logs.resolver';
 import { LogsService } from './logs.service';
 import { LogSchema } from './schemas/log';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Log', schema: LogSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: LOG_MODEL_NAME, schema: LogSchema }])
+  ],
   providers: [LogsService, LogsResolver],
   exports: [LogsService]
 })
--- libs/api/users/src/lib/users.module.ts
+++ libs/api/users/src/lib/users.module.ts
@@ -2,13 +2,15 @@ import { FilesModule } from '@bookapp/api/files';
 
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { USER_MODEL_NAME } from './constants';
 import { UserSchema } from './schemas/user';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: USER_MODEL_NAME, schema: UserSchema }]),
     FilesModule
   ],
   providers: [UsersService, UsersResolver],
