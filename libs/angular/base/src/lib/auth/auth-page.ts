83228673bea32da7d88f4cc6e659a867d8fee755
denlysenko <den.lysenko.mail@gmail.com>
2019-08-22 17:14:06 +0300

    [base] add base auth page

---
+++ libs/angular/base/src/lib/auth/auth-page.ts
@@ -0,0 +1,72 @@
+import { RouterExtensions } from '@bookapp/angular/core';
+import { AuthService } from '@bookapp/angular/data-access';
+import { SignupCredentials } from '@bookapp/shared/models';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+
+export abstract class AuthPageBase {
+  private errors = new BehaviorSubject<any | null>(null);
+  private loading = new BehaviorSubject<boolean>(false);
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly routerExtensions: RouterExtensions
+  ) {}
+
+  get loading$(): Observable<boolean> {
+    return this.loading.asObservable();
+  }
+
+  get errors$(): Observable<any | null> {
+    return this.errors.asObservable();
+  }
+
+  submit({ isLoggingIn, credentials }) {
+    isLoggingIn
+      ? this.login(credentials.email, credentials.password)
+      : this.signup(credentials);
+  }
+
+  private login(email: string, password: string) {
+    this.loading.next(true);
+    this.authService
+      .login(email, password)
+      .pipe(
+        finalize(() => {
+          this.loading.next(false);
+        })
+      )
+      .subscribe(this.onNext.bind(this));
+  }
+
+  private signup(credentials: SignupCredentials) {
+    this.loading.next(true);
+    this.authService
+      .signup(credentials)
+      .pipe(
+        finalize(() => {
+          this.loading.next(false);
+        })
+      )
+      .subscribe(this.onNext.bind(this));
+  }
+
+  private onNext({ data, errors }) {
+    if (data) {
+      this.routerExtensions.navigate([''], {
+        // for nativescript
+        clearHistory: true,
+        transition: {
+          name: 'flip',
+          duration: 300,
+          curve: 'linear'
+        }
+      });
+    }
+
+    if (errors) {
+      this.errors.next(errors);
+    }
+  }
+}
