b8a5deeac5982674d35e1bedb998960cfb442250
denlysenko <den.lysenko.mail@gmail.com>
2019-08-27 17:34:12 +0300

    [angular base] add main layout base component

--- libs/angular/base/src/index.ts
+++ libs/angular/base/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/core/base-form';
 
 export * from './lib/auth/auth-page';
 export * from './lib/auth/auth-form';
+
+export * from './lib/main-layout/main-layout';
---
+++ libs/angular/base/src/lib/main-layout/main-layout.ts
@@ -0,0 +1,71 @@
+import { OnDestroy } from '@angular/core';
+
+import { AuthService, LogsService } from '@bookapp/angular/data-access';
+import { LOG_CREATED_SUBSCRIPTION } from '@bookapp/shared/queries';
+
+import { map, tap } from 'rxjs/operators';
+
+import { BaseComponent } from '../core/base-component';
+
+export abstract class MainLayoutBase extends BaseComponent
+  implements OnDestroy {
+  user$ = this.authService.me().valueChanges.pipe(
+    map(({ data }) => data.me),
+    tap(user => {
+      if (!this.unsubscribeFromNewLogs) {
+        this.subscribeToNewLogs(user._id);
+      }
+    })
+  );
+
+  logsQueryRef = this.logsService.getLastLogs();
+
+  logs$ = this.logsQueryRef.valueChanges.pipe(
+    map(({ data }) => data.logs.rows)
+  );
+
+  private unsubscribeFromNewLogs: () => void | null = null;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly logsService: LogsService
+  ) {
+    super();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    if (this.unsubscribeFromNewLogs) {
+      this.unsubscribeFromNewLogs();
+      this.unsubscribeFromNewLogs = null;
+    }
+
+    super.ngOnDestroy();
+  }
+
+  private subscribeToNewLogs(userId: string) {
+    this.unsubscribeFromNewLogs = this.logsQueryRef.subscribeToMore({
+      document: LOG_CREATED_SUBSCRIPTION,
+      variables: { userId },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) {
+          return prev;
+        }
+
+        const newLogs = [subscriptionData.data.logCreated, ...prev.logs.rows];
+        newLogs.pop();
+
+        return {
+          logs: {
+            rows: newLogs,
+            count: prev.logs.count,
+            __typename: 'LogsResponse'
+          }
+        };
+      }
+    });
+  }
+}
