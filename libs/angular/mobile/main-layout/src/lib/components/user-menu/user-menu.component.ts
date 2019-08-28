d7884e7658bc4a8fa3b4fe6fe80786de1bf2e779
denlysenko <den.lysenko.mail@gmail.com>
2019-08-28 12:13:07 +0300

    [angular mobile main] add user menu

---
+++ libs/angular/mobile/main-layout/src/lib/components/user-menu/user-menu.component.ts
@@ -0,0 +1,21 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output
+} from '@angular/core';
+import { userMenu } from '@bookapp/angular/shared';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bookapp-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserMenuComponent {
+  readonly userMenu = userMenu;
+
+  @Output()
+  logout = new EventEmitter<void>();
+}
