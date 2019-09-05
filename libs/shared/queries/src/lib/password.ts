8bff19708479405d09d6b907c85db6cd8cd1b858
denlysenko <den.lysenko.mail@gmail.com>
2019-09-05 17:06:29 +0300

    [shared] change arg name

--- libs/shared/queries/src/lib/password.ts
+++ libs/shared/queries/src/lib/password.ts
@@ -2,6 +2,6 @@ import gql from 'graphql-tag';
 
 export const CHANGE_PASSWORD_MUTATION = gql`
   mutation changePassword($newPassword: String!, $oldPassword: String!) {
-    changePassword(newPassword: $newPassword, oldPassword: $oldPassword)
+    changePassword(password: $newPassword, oldPassword: $oldPassword)
   }
 `;
