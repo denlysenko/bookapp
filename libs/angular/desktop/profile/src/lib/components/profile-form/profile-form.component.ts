0ac5fa622b6fac9a49cd528c8cd6935a109e9479
denlysenko <den.lysenko.mail@gmail.com>
2019-09-06 11:58:30 +0300

    [angular desktop profile] add profile form

--- libs/angular/desktop/profile/src/lib/components/profile-form/profile-form.component.ts
+++ libs/angular/desktop/profile/src/lib/components/profile-form/profile-form.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { BaseForm } from '@bookapp/angular/base';
+import { FeedbackPlatformService } from '@bookapp/angular/core';
+import { User } from '@bookapp/shared/models';
+
+interface ProfileForm {
+  id: any;
+  user: Partial<User>;
+}
 
 @Component({
   selector: 'bookapp-profile-form',
@@ -6,11 +22,48 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./profile-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileFormComponent implements OnInit {
+export class ProfileFormComponent extends BaseForm {
+  form = this.fb.group({
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]]
+  });
 
-  constructor() { }
+  @Input() loading: boolean;
 
-  ngOnInit() {
+  @Input()
+  set user(user: User) {
+    if (user) {
+      this._userId = user._id;
+      this.form.patchValue(user);
+    }
   }
 
+  @Input()
+  set error(error: any) {
+    if (error) {
+      this.handleError(error);
+    }
+  }
+
+  @Output() formSubmitted = new EventEmitter<ProfileForm>();
+
+  private _userId: any;
+
+  constructor(
+    feedbackService: FeedbackPlatformService,
+    private readonly fb: FormBuilder
+  ) {
+    super(feedbackService);
+  }
+
+  get userId() {
+    return this._userId;
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.formSubmitted.emit({ id: this.userId, user: this.form.value });
+    }
+  }
 }
--- libs/angular/desktop/profile/src/lib/profile.module.ts
+++ libs/angular/desktop/profile/src/lib/profile.module.ts
@@ -1,12 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { ProfileFormComponent } from './components/profile-form/profile-form.component';
 import { ProfilePageComponent } from './containers/profile-page/profile-page.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   declarations: [ProfileFormComponent, ProfilePageComponent]
 })
 export class ProfileModule {}
