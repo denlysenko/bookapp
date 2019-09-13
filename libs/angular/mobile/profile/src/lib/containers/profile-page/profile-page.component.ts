11112f6573f48a810794f55e388a797d844b722a
denlysenko <den.lysenko.mail@gmail.com>
2019-09-13 16:50:59 +0300

    [angular mobile] add profile page

---
+++ libs/angular/mobile/profile/src/lib/containers/profile-page/profile-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { ProfilePageBase } from '@bookapp/angular/base';
+import { FeedbackPlatformService } from '@bookapp/angular/core';
+import { AuthService, ProfileService } from '@bookapp/angular/data-access';
+
+@Component({
+  selector: 'bookapp-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent extends ProfilePageBase {
+  constructor(
+    profileService: ProfileService,
+    authService: AuthService,
+    feedbackService: FeedbackPlatformService
+  ) {
+    super(profileService, authService, feedbackService);
+  }
+}
--- libs/angular/mobile/profile/src/lib/profile.module.ts
+++ libs/angular/mobile/profile/src/lib/profile.module.ts
@@ -4,16 +4,20 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { NgShadowModule } from 'nativescript-ngx-shadow';
 import { NativeScriptUIDataFormModule } from 'nativescript-ui-dataform/angular';
+
 import { ProfileFormComponent } from './components/profile-form/profile-form.component';
+import { ProfilePageComponent } from './containers/profile-page/profile-page.component';
+import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   imports: [
     NativeScriptCommonModule,
     CommonModule,
+    ProfileRoutingModule,
     NativeScriptUIDataFormModule,
     NgShadowModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  declarations: [ProfileFormComponent]
+  declarations: [ProfileFormComponent, ProfilePageComponent]
 })
 export class ProfileModule {}
