d53b1e66d8dc4315ad2b88490103ac70114f2cf5
denlysenko <den.lysenko.mail@gmail.com>
2019-09-15 21:38:16 +0300

    add ui desktop folder

--- libs/angular/ui/src/index.ts
+++ libs/angular/ui/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/image-selector/image-selector.module';
-export * from './lib/image-selector/image-selector.component';
-
-export * from './lib/drop/drop.module';
+export * from './lib/desktop/image-selector/image-selector.module';
+export * from './lib/desktop/image-selector/image-selector.component';
+export * from './lib/desktop/drop/drop.module';
