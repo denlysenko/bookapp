c2de096d660116037f1a72772830b1503f32aed6
denlysenko <den.lysenko.mail@gmail.com>
2019-09-16 16:03:09 +0300

    add roles guard

---
+++ libs/angular/shared/src/lib/guards/roles.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route
+} from '@angular/router';
+
+import { RouterExtensions } from '@bookapp/angular/core';
+import { AuthService } from '@bookapp/angular/data-access';
+
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+@Injectable()
+export class RolesGuard implements CanActivate, CanLoad {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly routerExtensions: RouterExtensions
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.hasRoles(route.data['roles']);
+  }
+
+  canLoad(route: Route): Observable<boolean> {
+    return this.hasRoles(route.data['roles']);
+  }
+
+  private hasRoles(roles: string[]): Observable<boolean> {
+    return this.authService.me().valueChanges.pipe(
+      map(({ data }) => data.me),
+      map(user => {
+        if (!user.roles.some(role => roles.includes(role))) {
+          this.routerExtensions.navigate(['']);
+          return false;
+        }
+
+        return true;
+      }),
+      take(1)
+    );
+  }
+}
