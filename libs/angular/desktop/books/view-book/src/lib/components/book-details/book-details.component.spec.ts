184103657500cabe20eb1bd2c7140b0d99d540f3
denlysenko <den.lysenko.mail@gmail.com>
2019-10-07 21:32:36 +0300

    [angular desktop] add book details

---
+++ libs/angular/desktop/books/view-book/src/lib/components/book-details/book-details.component.spec.ts
@@ -0,0 +1,110 @@
+import { CommonModule } from '@angular/common';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { RatingModule } from '@bookapp/angular/ui-desktop';
+import { BOOKMARKS, ROLES } from '@bookapp/shared';
+import { book, user } from '@bookapp/testing';
+
+import { BookDetailsComponent } from './book-details.component';
+
+describe('BookDetailsComponent', () => {
+  let component: BookDetailsComponent;
+  let fixture: ComponentFixture<BookDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        FormsModule,
+        RatingModule,
+        MatIconModule,
+        RouterTestingModule
+      ],
+      declarations: [BookDetailsComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookDetailsComponent);
+    component = fixture.componentInstance;
+    component.book = book;
+    component.bookmarks = [];
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('bookmarkAdded', () => {
+    let button: HTMLButtonElement;
+
+    beforeEach(() => {
+      jest.spyOn(component.bookmarkAdded, 'emit');
+      button = fixture.nativeElement.querySelector('#favorites');
+      button.click();
+    });
+
+    it('should emit bookmarkAdded event', () => {
+      expect(component.bookmarkAdded.emit).toHaveBeenCalledWith({
+        type: BOOKMARKS.FAVORITES,
+        bookId: book._id
+      });
+    });
+  });
+
+  describe('bookmarkRemoved', () => {
+    let button: HTMLButtonElement;
+
+    beforeEach(() => {
+      jest.spyOn(component.bookmarkRemoved, 'emit');
+      component.bookmarks = [BOOKMARKS.FAVORITES];
+      fixture.detectChanges();
+      button = fixture.nativeElement.querySelector('#favorites');
+      button.click();
+    });
+
+    it('should emit bookmarkRemoved event', () => {
+      expect(component.bookmarkRemoved.emit).toHaveBeenCalledWith({
+        type: BOOKMARKS.FAVORITES,
+        bookId: book._id
+      });
+    });
+  });
+
+  describe('rate()', () => {
+    it('should emit bookRated event', () => {
+      jest.spyOn(component.bookRated, 'emit');
+
+      const starEl = fixture.debugElement.nativeElement.querySelectorAll(
+        '.rating-star'
+      )[2];
+
+      starEl.click();
+      fixture.detectChanges();
+
+      expect(component.bookRated.emit).toHaveBeenCalledWith({
+        bookId: book._id,
+        rate: 3
+      });
+    });
+  });
+
+  describe('isAdmin', () => {
+    it('should hide edit link', () => {
+      expect(fixture.nativeElement.querySelector('#edit')).toBeNull();
+    });
+
+    it('should show edit link', () => {
+      component.user = { ...user, roles: [ROLES.ADMIN] };
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.querySelector('#edit')).not.toBeNull();
+    });
+  });
+});
---
+++ libs/angular/desktop/books/view-book/src/lib/components/book-details/book-details.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { BookDetailsBase } from '@bookapp/angular/base';
+import { ROLES, User } from '@bookapp/shared';
+
+@Component({
+  selector: 'bookapp-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BookDetailsComponent extends BookDetailsBase {
+  @Input()
+  set user(user: User) {
+    if (user) {
+      this._isAdmin = user.roles.includes(ROLES.ADMIN);
+    }
+  }
+
+  private _isAdmin: boolean;
+
+  get isAdmin(): boolean {
+    return this._isAdmin;
+  }
+}
