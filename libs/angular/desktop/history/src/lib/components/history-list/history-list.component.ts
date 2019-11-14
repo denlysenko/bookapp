de224b6eab1b6f39995c007854f3c11d661cff05
denlysenko <den.lysenko.mail@gmail.com>
2019-11-14 22:11:14 +0200

    [angular desktop] add history list component

--- libs/angular/desktop/history/src/lib/components/history-list/history-list.component.ts
+++ libs/angular/desktop/history/src/lib/components/history-list/history-list.component.ts
@@ -1,4 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
+
+import { DEFAULT_LIMIT } from '@bookapp/angular/core';
+import { Log, UserActionsDesc } from '@bookapp/shared';
 
 @Component({
   selector: 'bookapp-history-list',
@@ -6,8 +17,24 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./history-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HistoryListComponent implements OnInit {
-  constructor() {}
+export class HistoryListComponent {
+  readonly displayedColumns = ['createdAt', 'action', 'book'];
+  readonly defaultLimit = DEFAULT_LIMIT;
+  readonly actions = UserActionsDesc;
+  readonly sorting = {
+    active: 'createdAt',
+    direction: 'desc'
+  };
 
-  ngOnInit() {}
+  @Input()
+  logs: Log[];
+
+  @Input()
+  totalCount: number;
+
+  @Output()
+  sortChanged = new EventEmitter<Sort>();
+
+  @Output()
+  pageChanged = new EventEmitter<PageEvent>();
 }
--- libs/angular/desktop/history/src/lib/history.module.ts
+++ libs/angular/desktop/history/src/lib/history.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 import { HistoryListComponent } from './components/history-list/history-list.component';
 import { HistoryPageComponent } from './containers/history-page/history-page.component';
 import { HistoryRoutingModule } from './history-routing.module';
 
 @NgModule({
-  imports: [CommonModule, HistoryRoutingModule],
+  imports: [
+    CommonModule,
+    HistoryRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
   declarations: [HistoryPageComponent, HistoryListComponent]
 })
 export class HistoryModule {}
