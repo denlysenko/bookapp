b38591baee62f6f353493e69bb339646cd81d3ac
denlysenko <den.lysenko.mail@gmail.com>
2019-11-17 16:03:28 +0200

    [angular desktop] add history list tests

--- libs/angular/desktop/history/src/lib/components/history-list/history-list.component.spec.ts
+++ libs/angular/desktop/history/src/lib/components/history-list/history-list.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HAMMER_LOADER } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { HistoryModule } from '../../history.module';
 import { HistoryListComponent } from './history-list.component';
 
 describe('HistoryListComponent', () => {
@@ -8,7 +12,13 @@ describe('HistoryListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HistoryListComponent]
+      imports: [HistoryModule, NoopAnimationsModule, RouterTestingModule],
+      providers: [
+        {
+          provide: HAMMER_LOADER,
+          useValue: () => new Promise(() => {})
+        }
+      ]
     }).compileComponents();
   }));
 
@@ -20,5 +30,20 @@ describe('HistoryListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('sortChanged()', () => {
+    beforeEach(() => {
+      jest.spyOn(component.sortChanged, 'emit');
+
+      const sortHeader = fixture.debugElement.nativeElement.querySelector(
+        '.mat-sort-header-button'
+      );
+      sortHeader.click();
+    });
+
+    it('should emit sortChanged event', () => {
+      expect(component.sortChanged.emit).toHaveBeenCalled();
+    });
   });
 });
